cefe5a1431e8f13386197c0a0df20bc365b6bf3f
chison2707 <chiphansonzz17@gmail.com>
2024-09-23 16:20:13 +0700

    Tính năng phân trang

--- controllers/admin/product.cotroller.js
+++ controllers/admin/product.cotroller.js
@@ -19,12 +19,29 @@ module.exports.index = async (req, res) => {
         find.title = objSearch.regex;
     }
 
-    const products = await Product.find(find);
+    // pagination
+    let objPagination = {
+        currentPage: 1,
+        limitItems: 4
+    }
+    if (req.query.page) {
+        objPagination.currentPage = parseInt(req.query.page);
+    }
+
+    objPagination.skip = (objPagination.currentPage - 1) * objPagination.limitItems;
+
+    const countProducts = await Product.countDocuments(find);
+    const totalPage = Math.ceil(countProducts / objPagination.limitItems);
+    objPagination.totalPage = totalPage;
+    // end pagination
+
+    const products = await Product.find(find).limit(objPagination.limitItems).skip(objPagination.skip);
     // console.log(products);
     res.render("admin/pages/products/index", {
         pageTitle: "Danh sách sản phẩm",
         products: products,
         filterStatus: filterStatus,
-        keyword: objSearch.keyword
+        keyword: objSearch.keyword,
+        pagination: objPagination
     });
 }
