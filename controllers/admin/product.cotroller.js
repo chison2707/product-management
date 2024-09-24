3711d1c517be789d31361c9ba7a55b4981d8ebdf
chison2707 <chiphansonzz17@gmail.com>
2024-09-24 21:32:57 +0700

    Thay đổi trạng thái một sản phẩm

--- controllers/admin/product.cotroller.js
+++ controllers/admin/product.cotroller.js
@@ -42,3 +42,13 @@ module.exports.index = async (req, res) => {
         pagination: objPagination
     });
 }
+
+//[GET] / admin/prducts/change-status/:status/:id
+module.exports.changeStatus = async (req, res) => {
+    const status = req.params.status;
+    const id = req.params.id;
+
+    await Product.updateOne({ _id: id }, { status: status });
+
+    res.redirect("back");
+}
