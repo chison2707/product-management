28a04f2a693a5fd21633303bcd622551ec00a07c
chison2707 <chiphansonzz17@gmail.com>
2024-09-25 16:28:32 +0700

    thay đổi trạng thái nhiều sản phẩm

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -54,3 +54,60 @@ if (buttonPagination) {
     })
 };
 // end pagination
+
+// checkbox multi
+const checkboxMulti = document.querySelector("[checkbox-multi]");
+if (checkboxMulti) {
+    const inputCheckAll = checkboxMulti.querySelector("input[name='checkall']");
+    const inputsId = checkboxMulti.querySelectorAll("input[name='id']");
+    inputCheckAll.addEventListener("click", () => {
+        if (inputCheckAll.checked) {
+            inputsId.forEach(input => {
+                input.checked = true;
+            });
+        } else {
+            inputsId.forEach(input => {
+                input.checked = false;
+            });
+        }
+    });
+
+    inputsId.forEach((input) => {
+        input.addEventListener("click", () => {
+            const countChecked = checkboxMulti.querySelectorAll("input[name='id']:checked").length;
+            if (countChecked == inputsId.length) {
+                inputCheckAll.checked = true;
+            } else {
+                inputCheckAll.checked = false;
+            }
+        });
+    });
+}
+// end checkbox multi
+
+// form change multi
+const formChangeMulti = document.querySelector("[form-change-multi]");
+if (formChangeMulti) {
+    formChangeMulti.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const checkboxMulti = document.querySelector("[checkbox-multi]");
+        const inputsChecked = checkboxMulti.querySelectorAll("input[name='id']:checked");
+
+        if (inputsChecked.length > 0) {
+            let ids = [];
+            const inputIds = formChangeMulti.querySelector("input[name='ids']");
+            inputsChecked.forEach(input => {
+                const id = input.value;
+                ids.push(id);
+            })
+
+            inputIds.value = ids.join(", ");
+
+            formChangeMulti.submit();
+        } else {
+            alert(" Vui lòng chọn ít nhất một bản ghi");
+        }
+    });
+}
+// end form change multi
