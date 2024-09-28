899600bfd6f65012240612db0b985d8c5f7d6ebe
chison2707 <chiphansonzz17@gmail.com>
2024-09-28 09:43:31 +0700

    xóa vĩnh viễn

--- controllers/admin/product.cotroller.js
+++ controllers/admin/product.cotroller.js
@@ -71,3 +71,13 @@ module.exports.changeMulti = async (req, res) => {
 
     res.redirect("back");
 }
+
+//[PATCH] / admin/prducts/delete/:id
+module.exports.deleteItem = async (req, res) => {
+    const status = req.params.status;
+    const id = req.params.id;
+
+    await Product.deleteOne({ _id: id });
+
+    res.redirect("back");
+}
--- public/admin/js/product.js
+++ public/admin/js/product.js
@@ -24,3 +24,26 @@ if (buttonsChangeStatus.length > 0) {
     });
 }
 // end change status
+
+
+// delete item
+const buttonsDelete = document.querySelectorAll("[button-delete]");
+if (buttonsDelete.length > 0) {
+    const formDeleteItem = document.querySelector("#form-delete-item");
+    const path = formDeleteItem.getAttribute("data-path");
+    buttonsDelete.forEach(button => {
+        button.addEventListener("click", () => {
+            const isConfirm = confirm("Bạn có chắc chắn muốn xóa sản phẩm này không?");
+
+            if (isConfirm) {
+                const id = button.getAttribute("data-id");
+
+                const action = `${path}/${id}?_method=DELETE`;
+
+                formDeleteItem.action = action;
+                formDeleteItem.submit();
+            }
+        });
+    });
+}
+// end delete item
