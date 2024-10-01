16e3fff90e96e865c205f4600c4c71170a9c2981
chison2707 <chiphansonzz17@gmail.com>
2024-10-01 10:24:01 +0700

    preview image

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -141,5 +141,26 @@ if (showAlert) {
         showAlert.classList.add("alert-hidden");
     })
 }
-
 // end show alert
+
+// upload image
+const uploadImage = document.querySelector("[upload-image]");
+if (uploadImage) {
+    const uploadImageInput = document.querySelector("[upload-image-input]");
+    const uploadImagePreview = document.querySelector("[upload-image-preview]");
+    const removeImage = document.querySelector("[removeImage]");
+
+    uploadImageInput.addEventListener("change", (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            uploadImagePreview.src = URL.createObjectURL(file);
+            removeImage.style.display = 'block';
+        }
+    });
+    removeImage.addEventListener("click", () => {
+        uploadImageInput.value = '';
+        uploadImagePreview.src = '';
+        removeImage.style.display = 'none';
+    })
+}
+// end upload image
