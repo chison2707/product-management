5c93ad7af78901c7adb7f69b0e1171006052b513
chison2707 <chiphansonzz17@gmail.com>
2024-10-02 16:51:02 +0700

    config deploy onl

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(methodOverride('_method'))
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.set('views', './views');
+app.set('views', `${__dirname}/views`);
 app.set('view engine', 'pug');
 
 // flash
@@ -34,7 +34,7 @@ app.use(flash());
 //app local variables
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
-app.use(express.static('public'));
+app.use(express.static(`${__dirname}/public`));
 
 route(app);
 routeAdmin(app);
