21717b2efa2b4a42f3ea3deaf2e413f480a2882f
chison2707 <chiphansonzz17@gmail.com>
2024-10-07 10:52:19 +0700

    Xây dựng phần Danh sách nhóm quyền

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -2,6 +2,7 @@ const systemConfig = require("../../config/system");
 const dashboardRoutes = require("./dashboard.route");
 const productRoutes = require("./product.route");
 const productCategoryRoutes = require("./product-category.route");
+const roleRoutes = require("./role.route");
 
 module.exports = (app) => {
     const PARTH_ADMIN = systemConfig.prefixAdmin;
@@ -9,4 +10,5 @@ module.exports = (app) => {
 
     app.use(PARTH_ADMIN + "/products", productRoutes);
     app.use(PARTH_ADMIN + "/products-category", productCategoryRoutes);
+    app.use(PARTH_ADMIN + "/roles", roleRoutes);
 }
