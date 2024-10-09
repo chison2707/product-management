c9cc020132827ffa0a8c02b1a09f34de3aadb148
chison2707 <chiphansonzz17@gmail.com>
2024-10-09 16:19:46 +0700

    Xây dựng phần phân quyền

---
+++ public/admin/js/role.js
@@ -0,0 +1,61 @@
+// permissions
+const tablePermissions = document.querySelector("[table-permissions]");
+if (tablePermissions) {
+    const buttonSubmit = document.querySelector("[button-submit]");
+    buttonSubmit.addEventListener("click", () => {
+        let permissions = [];
+        const rows = tablePermissions.querySelectorAll("[data-name]");
+
+        rows.forEach((row) => {
+            const name = row.getAttribute("data-name");
+            const ipnputs = row.querySelectorAll("input");
+
+            if (name == "id") {
+                ipnputs.forEach(input => {
+                    const id = input.value;
+                    permissions.push({
+                        id: id,
+                        permissions: []
+                    });
+                })
+            } else {
+                ipnputs.forEach((input, index) => {
+                    const checked = input.checked;
+
+                    if (checked) {
+                        permissions[index].permissions.push(name);
+                    }
+                });
+            }
+        });
+        console.log(permissions);
+
+        if (permissions.length > 0) {
+            const formChangePermissions = document.querySelector("#form-change-permissions");
+            const inputPermissions = formChangePermissions.querySelector("input[name='permissions']");
+
+            inputPermissions.value = JSON.stringify(permissions);
+            formChangePermissions.submit();
+        }
+    })
+};
+// end permissions
+
+// permissions data default
+const dataRecords = document.querySelector("[data-records]");
+if (dataRecords) {
+    const records = JSON.parse(dataRecords.getAttribute("data-records"));
+    const tablePermissions = document.querySelector("[table-permissions]");
+
+    records.forEach((record, index) => {
+        const permissions = record.permissions;
+
+        permissions.forEach(permission => {
+            const row = tablePermissions.querySelector(`[data-name="${permission}"]`);
+            const inpput = row.querySelectorAll("input")[index];
+
+            inpput.checked = true;
+        });
+    })
+}
+// end permissions data default
