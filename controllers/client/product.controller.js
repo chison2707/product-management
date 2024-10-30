f7082add86f062804ee4afb7fab97e1019303e84
chison2707 <chiphansonzz17@gmail.com>
2024-10-30 11:14:05 +0700

    trang danh sách sản phẩm bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,8 @@
 const Product = require("../../models/product.model");
+const productCategory = require("../../models/product-category.model");
+
 const productsHelper = require("../../helpers/products");
+const productsCategoryHelper = require("../../helpers/products-category");
 
 //[GET] / products
 module.exports.index = async (req, res) => {
@@ -16,7 +19,7 @@ module.exports.index = async (req, res) => {
     });
 }
 
-//[GET] / products
+//[GET] / products/:slug
 module.exports.detail = async (req, res) => {
     try {
         const find = {
@@ -35,4 +38,29 @@ module.exports.detail = async (req, res) => {
         res.redirect(`/products`);
     }
 
+}
+
+//[GET] / products/:slugCategory
+module.exports.category = async (req, res) => {
+
+    const category = await productCategory.findOne({
+        slug: req.params.slugCategory,
+        status: "active",
+        deleted: false
+    });
+
+    const listSubCategory = await productsCategoryHelper.getSubCategory(category.id);
+
+    const listSubCategoryId = listSubCategory.map(item => item.id);
+
+    const products = await Product.find({
+        product_category_id: { $in: [category.id, ...listSubCategoryId] },
+        deleted: false
+    }).sort({ position: "desc" });
+    const newProducts = productsHelper.priceNewProducts(products);
+
+    res.render("client/pages/products/index", {
+        pageTitle: category.title,
+        products: newProducts
+    });
 }
