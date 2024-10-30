7ce33da013ea2e7e3b53d4a895d951040553804d
chison2707 <chiphansonzz17@gmail.com>
2024-10-30 09:35:59 +0700

    Hiển thị danh sách sản phẩm mới

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -12,8 +12,17 @@ module.exports.index = async (req, res) => {
 
     const newProducts = productsHelper.priceNewProducts(productFeatured);
 
+    // Lấy ra sản phẩm mới nhất
+    const productsNew = await Product.find({
+        deleted: false,
+        status: "active"
+    }).sort({ position: "desc" }).limit(4);
+
+    const newProductsNew = productsHelper.priceNewProducts(productsNew);
+
     res.render("client/pages/home/index", {
         pageTitle: "Trang chủ",
-        productFeatured: newProducts
+        productFeatured: newProducts,
+        productsNew: newProductsNew
     });
 }
