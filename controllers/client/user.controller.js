98808a6b5d871cc4a086fe881ce7ffdd400be6a8
chison2707 <chiphansonzz17@gmail.com>
2024-11-05 21:30:47 +0700

    Tính năng đăng nhập

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -28,3 +28,40 @@ module.exports.registerPost = async (req, res) => {
 
     res.redirect("/");
 }
+
+// [GET]/user/login
+module.exports.login = async (req, res) => {
+    res.render("client/pages/user/login", {
+        pageTitle: "Đăng nhập"
+    });
+}
+
+// [POST]/user/login
+module.exports.loginPost = async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const user = await User.findOne({
+        email: email,
+        deleted: false
+    });
+    if (!user) {
+        req.flash("error", "Email không tồn tại");
+        res.redirect("back");
+        return;
+    }
+
+    if (md5(password) != user.password) {
+        req.flash("error", "Mật khẩu không đúng!");
+        res.redirect("back");
+        return;
+    }
+
+    if (user.status == "inactive") {
+        req.flash("error", "Tài khoản đã bị khóa!");
+        res.redirect("back");
+        return;
+    }
+
+    res.cookie("tokenUser", user.tokenUser);
+    res.redirect(`/`);
+}
