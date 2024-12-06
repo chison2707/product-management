a79e1f3e7829724f48d3137917e1cc70f022a16c
chison2707 <chiphansonzz17@gmail.com>
2024-12-06 10:34:05 +0700

    tính năng chat theo 2 người

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -3,12 +3,15 @@ const User = require("../../models/user.model");
 
 const chatSocket = require("../../sockets/client/chat.socket")
 
-// [GET]/chat
+// [GET]/chat/:roomChatId
 module.exports.index = async (req, res) => {
-    chatSocket(res);
+    const roomChatId = req.params.roomChatId;
+
+    chatSocket(req, res);
 
     // Lấy ra data
     const chats = await Chat.find({
+        room_chat_id: roomChatId,
         deleted: false
     });
     for (const chat of chats) {
--- sockets/client/chat.socket.js
+++ sockets/client/chat.socket.js
@@ -1,10 +1,14 @@
 const Chat = require("../../models/chat.model");
 const uploadToCloudinary = require("../../helpers/uploadToCloudinary");
 
-module.exports = async (res) => {
+module.exports = async (req, res) => {
     const userId = res.locals.user._id.toString();
     const fullName = res.locals.user.fullName;
+    const roomChatId = req.params.roomChatId;
+
     _io.once('connection', (socket) => {
+        socket.join(roomChatId);
+
         socket.on("CLIENT_SEND_MESSAGE", async (data) => {
             let images = [];
             for (const imageBuffer of data.images) {
@@ -15,13 +19,14 @@ module.exports = async (res) => {
             // lưu vào database
             const chat = new Chat({
                 user_id: userId,
+                room_chat_id: roomChatId,
                 content: data.content,
                 images: images
             });
             await chat.save();
 
             // trả data về client
-            _io.emit("SERVER_RETURN_MESSAGE", {
+            _io.to(roomChatId).emit("SERVER_RETURN_MESSAGE", {
                 user_id: userId,
                 fullName: fullName,
                 content: data.content,
@@ -29,7 +34,7 @@ module.exports = async (res) => {
             });
         })
         socket.on("CLIENT_SEND_TYPING", (type) => {
-            socket.broadcast.emit("SERVER_RETURN_TYPING", {
+            socket.broadcast.to(roomChatId).emit("SERVER_RETURN_TYPING", {
                 userId: userId,
                 fullName: fullName,
                 type: type
